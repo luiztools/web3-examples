7c6d8453e309b993ad98da1ffb04f42939ce0999
LuizTools <luizfduartejr@gmail.com>
2022-10-10 14:47:43 -0300

    dapp-react commit

---
+++ dapp-react/README.md
@@ -0,0 +1,16 @@
+# dapp-react
+
+A simple web frontend written with JS/React to integrate with a smart contract in the blockchain (BSC Testnet).
+
+Smart Contract address: https://testnet.bscscan.com/address/0xE9956c971B72aD74F249E616828df613F03E858b
+
+## How to Run
+1. git clone
+2. npm install
+3. npm start
+
+## Referências
+
+Tutorial em (inclui vídeo): https://www.luiztools.com.br/post/como-criar-um-frontend-web-para-smart-contract-com-reactjs/
+
+Me siga nas redes sociais: https://about.me/luiztools
---
+++ dapp-react/src/App.js
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import ABI from './abi.json';
+import './App.css';
+
+function App() {
+
+  const [customerId, setCustomerId] = useState("0");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [error, setError] = useState("");
+
+  const CONTRACT_ADDRESS = "0xE9956c971B72aD74F249E616828df613F03E858b";
+
+  async function getProvider() {
+    if (!window.ethereum) throw new Error(`No MetaMask found!`);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const accounts = await provider.send("eth_requestAccounts", []);
+    if (!accounts || !accounts.length) throw new Error('Wallet not found/allowed!');
+    return provider;
+  }
+
+  async function doSearch() {
+    try {
+      const provider = await getProvider();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+      const customer = await contract.getCustomer(customerId);
+      alert(JSON.stringify(customer));
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  async function getContractSigner() {
+    const provider = await getProvider();
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+    return contract.connect(signer);
+  }
+
+  async function doSave() {
+    try {
+      const contract = await getContractSigner();
+      const tx = await contract.addCustomer({ name, age });
+      alert(JSON.stringify(tx));
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  function onSearchClick() {
+    setError('');
+    doSearch();
+  }
+
+  function onSaveClick() {
+    setError('');
+    doSave();
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <label>Customer ID: <input type="number" value={customerId} onChange={(evt) => setCustomerId(evt.target.value)} /></label>
+        </p>
+        <p>
+          <input type="button" value="Search" onClick={onSearchClick} />
+        </p>
+        <hr />
+        <p>
+          <label>Name: <input type="text" value={name} onChange={(evt) => setName(evt.target.value)} /></label>
+        </p>
+        <p>
+          <label>Age: <input type="number" value={age} onChange={(evt) => setAge(evt.target.value)} /></label>
+        </p>
+        <p>
+          <input type="button" value="Save" onClick={onSaveClick} />
+        </p>
+        <p>
+          {error}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
