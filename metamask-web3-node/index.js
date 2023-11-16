43c4ea28e21e4dbd5790bf1fa64ebe5d69888031
LuizTools <luizfduartejr@gmail.com>
2023-11-16 00:28:04 -0300

    update to web3.js v4.2.2

--- metamask-web3-node/index.js
+++ metamask-web3-node/index.js
@@ -7,6 +7,7 @@ const web3 = new Web3(process.env.INFURA_URL);
 //additional lines if you want to send transactions
 const account = web3.eth.accounts.privateKeyToAccount('0x' + process.env.PRIVATE_KEY);
 web3.eth.accounts.wallet.add(account);
+web3.eth.defaultAccount = account.address;
 
 async function getPrcBalance(from) {
     const contract = new web3.eth.Contract(ABI, process.env.CONTRACT_ADDRESS);
