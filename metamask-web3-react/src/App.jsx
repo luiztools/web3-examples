d9bd400b302f623073c0054c046b73e6efa6c3ef
LuizTools <luizfduartejr@gmail.com>
2026-01-07 18:56:14 -0300

    update CRA > Vite

--- metamask-web3-react/src/App.jsx
+++ metamask-web3-react/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
-import { getEthBalance, transferEth } from './MetaMaskService';
+import { getEthBalance } from './MetaMaskService';
 
 function App() {
 
   const [address, setAddress] = useState("0xE4ffEEd88111e1DFCc3a852d9334C65e38BF2880");
   const [balance, setBalance] = useState('');
-  const [toAddress, setToAddress] = useState("");
-  const [quantity, setQuantity] = useState("");
   const [message, setMessage] = useState('');
 
   async function checkBalance() {
@@ -14,17 +12,6 @@ function App() {
       setMessage("");
       let balance = await getEthBalance(address);
       setBalance(balance);
-    }
-    catch (err) {
-      setMessage(err.message);
-    }
-  }
-
-  async function transfer() {
-    try {
-      setMessage("");
-      let result = await transferEth(address, toAddress, quantity);
-      setMessage(JSON.stringify(result));
     }
     catch (err) {
       setMessage(err.message);
@@ -40,16 +27,6 @@ function App() {
         Balance: {balance}
       </p>
       <p><input type="button" value="Balance" onClick={evt => checkBalance()} /></p>
-      <hr />
-      <p>
-        To Address: <input type="text" onChange={evt => setToAddress(evt.target.value)} />
-      </p>
-      <p>
-        Qty: <input type="text" onChange={evt => setQuantity(evt.target.value)} />
-      </p>
-      <p>
-        <input type="button" value="Transfer" onClick={evt => transfer()} />
-      </p>
       <hr />
       <p>
         {message}
--- metamask-web3-react/src/Web3Service.js
+++ metamask-web3-react/src/Web3Service.js
@@ -4,7 +4,6 @@ async function getMetaMaskProvider() {
     if (!window.ethereum) throw new Error(`No MetaMask found!`);
 
     const web3 = new Web3(window.ethereum); 
-    
     const accounts = await web3.eth.requestAccounts(); 
     if (!accounts || !accounts.length) throw new Error('Wallet not found/allowed!');
 
